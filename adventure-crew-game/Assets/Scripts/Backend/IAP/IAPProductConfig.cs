787f661d38b3ead6ab5872865d45eca305204505
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-03-21 21:55:09 -0400

    Added Non cunsumable IAP functionality

---
+++ adventure-crew-game/Assets/Scripts/Backend/IAP/IAPProductConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Backend.IAP
+{
+    [CreateAssetMenu(fileName = "IAP Product Config", menuName = "IAP/Product Config", order = 0)]
+    public class IAPProductConfig : ScriptableObject
+    {
+        [SerializeField] private IAPProduct[] products;
+
+        public IAPProduct[] Products => products;
+
+        internal IAPProduct GetProductById(string id)
+        {
+            return products.FirstOrDefault(product => product.Id == id);
+        }
+
+        internal bool IsProductPurchased(string id)
+        {
+            var product = GetProductById(id);
+            return product?.Purchased ?? false;
+        }
+
+        [ContextMenu("To JSON")]
+        public void ToJson()
+        {
+            var json = JsonUtility.ToJson(this, true);
+            Debug.Log(json);
+        }
+    }
+}
