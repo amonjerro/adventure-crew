c1b4baad1d08bd1920bc9d26215a15f1e6d5f2cf
amonjerro <eescudero_arens@hotmail.com>
2024-03-26 11:05:47 -0400

    Resolved stash conflicts

--- adventure-crew-game/Assets/Scripts/Combat/EnemyFormation.cs
+++ adventure-crew-game/Assets/Scripts/Combat/EnemyFormation.cs
@@ -1,13 +1,19 @@
+
+using System.Collections.Generic;
 using UnityEngine;
 
 
 [CreateAssetMenu(menuName = "ScriptableObjects/EnemyFormation")]
 public class EnemyFormation : ScriptableObject
 {
+    public List<FormationStruct> formation;
 
 }
 
 public struct FormationStruct
 {
+
+    public CombatStatsSO enemy;
+    public Vector3 position;
 
 }
