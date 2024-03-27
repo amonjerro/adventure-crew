ad63e594f080f770f5225eed69905236941483b7
amonjerro <eescudero_arens@hotmail.com>
2024-03-26 21:10:20 -0400

    Spawn Function Implemented

--- adventure-crew-game/Assets/Scripts/Combat/CombatEntityEnemy.cs
+++ adventure-crew-game/Assets/Scripts/Combat/CombatEntityEnemy.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class CombatEntityEnemy : CombatEntity
 {
+    public CombatStatsSO enemyStats;
     public int iniHP;
     public int maxHP;
     public int Damage;
     public int Agility;
     public float Range;
 
-    private void Awake()
+    private void Setup()
     {
         stats = new Stats(iniHP, maxHP, Damage, Agility, Range);
+    }
+
+    private void Awake()
+    {
+        Setup();
+    }
+
+    public void SetStats(CombatStatsSO statsData)
+    {
+        enemyStats = statsData;
+        stats = new Stats(enemyStats.stats.MaxHP, enemyStats.stats.MaxHP, enemyStats.stats.Damage, enemyStats.stats.Agility, enemyStats.stats.Range);
     }
 }
