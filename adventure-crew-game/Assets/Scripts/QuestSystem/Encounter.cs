c6009d9e8d2c2e51e61edcb68a215fdd0b79273e
amonjerro <eescudero_arens@hotmail.com>
2024-03-29 10:56:22 -0400

    Remove unwanted dependencies

--- adventure-crew-game/Assets/Scripts/QuestSystem/Encounter.cs
+++ adventure-crew-game/Assets/Scripts/QuestSystem/Encounter.cs
@@ -1,11 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class Encounter : ScriptableObject
 {
     public abstract void OnStart();
     public abstract void OnEnd();
-
 }
