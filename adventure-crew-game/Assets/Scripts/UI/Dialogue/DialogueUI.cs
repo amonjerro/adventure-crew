6f0b14e99e0805835409df2d5e85c4c178c1ca74
James Zilberman <jaz1324576@gmail.com>
2024-04-05 01:24:02 -0400

    Corrected Order

--- adventure-crew-game/Assets/Scripts/UI/Dialogue/DialogueUI.cs
+++ adventure-crew-game/Assets/Scripts/UI/Dialogue/DialogueUI.cs
@@ -69,10 +69,10 @@ public class DialogueUI : MonoBehaviour
     {
         if(charPosition == 0 && dialoguePosition >= config.Characters[0].Dialogue.Length) { onboarding.OnboardingTask("battle"); }
         else if (charPosition == 1 && dialoguePosition >= config.Characters[1].Dialogue.Length) { onboarding.OnboardingTask("roster"); }
-        else if(charPosition == 2 && dialoguePosition >= config.Characters[1].Dialogue.Length) { onboarding.OnboardingTask("contracts"); }
-        else if (charPosition == 3 && dialoguePosition >= config.Characters[2].Dialogue.Length) { onboarding.OnboardingTask("shop"); }
-        else if (charPosition == 4 && dialoguePosition < config.Characters[3].Dialogue.Length) { onboarding.OnboardingTask("map"); }
-        else if (charPosition == 4 && dialoguePosition >= config.Characters[3].Dialogue.Length) { onboarding.OnboardingTask("midland"); }
+        else if(charPosition == 2 && dialoguePosition >= config.Characters[2].Dialogue.Length) { onboarding.OnboardingTask("contracts"); }
+        else if (charPosition == 3 && dialoguePosition >= config.Characters[3].Dialogue.Length) { onboarding.OnboardingTask("shop"); }
+        else if (charPosition == 4 && dialoguePosition < config.Characters[4].Dialogue.Length) { onboarding.OnboardingTask("map"); }
+        else if (charPosition == 4 && dialoguePosition >= config.Characters[4].Dialogue.Length) { onboarding.OnboardingTask("midland"); }
         else if (charPosition == 5) { onboarding.OnboardingTask("quest"); }
     }
 
